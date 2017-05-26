9cf083663ce0eb869ee1c2f834cacab8df45c042
amartyashankha <amartyashankha@gmail.com>
2017-05-26 07:30:54 -0400

    Adde beamer shit

---
+++ L1/code/square/square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int N, square;
+    
+    cout << "Enter a number: ";
+    cin >> N;
+    square = N*N;
+    cout << "The square of " << N << " is " << square << endl;
+
+    return 0;
+
+}

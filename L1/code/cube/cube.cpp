b6d5516ebe8105b47edf385832fa6f34187eeb59
amartyashankha <amartyashankha@gmail.com>
2017-05-27 18:02:32 -0400

    Added Logic Flow Slides

--- L1/code/cube/cube.cpp
+++ L1/code/cube/cube.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main() {
 
-    double N, square;
+    double N, cube;
     
     cout << "Enter a number: ";
     cin >> N;       //Take input from user
-    square = N*N;   //Compute Square
-    cout << "The square of " << N << " is " << square << endl;
+    cube = N*N*N;   //Compute cube
+    cout << "The cube of " << N << " is " << cube << endl;
 
     return 0;
 
